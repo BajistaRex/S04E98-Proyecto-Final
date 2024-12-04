fe705dbf930af1bfd7e9ba90760aaac54f9805e4
BajistaRex <alanilt03@gmail.com>
2024-12-04 08:35:55 +0000

    🎸 Checkpoint 7 Fin

--- quasar-moneyballs/src/pages/PageEntries.vue
+++ quasar-moneyballs/src/pages/PageEntries.vue
@@ -5,10 +5,22 @@
       bordered
       separator
     >
-      <q-item
-        v-for="entry in entries"
-        :key="entry.id"
-      >
+
+    <q-slide-item 
+      v-for="entry in entries"
+      :key="entry.id"
+    @right="onEntrySlideRight($event, entry)"
+    left-color="positive" 
+    right-color="negative"
+    >
+    <!--   <template v-slot:left>
+          <q-icon name="done" />
+        </template> -->
+        <template v-slot:right>
+          <q-icon name="delete" />
+        </template>
+
+      <q-item>
         <q-item-section
         class="text-weight-bold"
           :class="useAmountColorClass(entry.amount)"
@@ -24,6 +36,9 @@
           {{ useCurrencify(entry.amount) }}
         </q-item-section>
       </q-item>
+
+</q-slide-item>
+
     </q-list>
   </div>
 
@@ -87,9 +102,17 @@
 */
 
 import { ref, computed, reactive } from 'vue'
-import { uid } from 'quasar'
+import { uid, useQuasar } from 'quasar'
 import { useCurrencify } from 'src/use/useCurrencify'
 import { useAmountColorClass } from 'src/use/useAmountColorClass'
+
+
+/*
+  quasar
+*/
+
+const $q = useQuasar()
+
 
 /*
   Entries
@@ -157,6 +180,51 @@ const newEntry = Object.assign({}, addEntryForm, { id: uid() })
   addEntryFormReset()
 }
 
+/*
+  Slide items
+*/
+
+const onEntrySlideRight = ({ reset }, entry) => {
+  $q.dialog({
+        title: 'Delete Entry',
+        message: `
+        Delete this entry?
+        <div class="q-mt-md text-weight-bold ${ useAmountColorClass(entry.amount) }">
+          ${ entry.name } :  ${ useCurrencify( entry.amount )}
+          </div>
+        `,
+        cancel: true,
+        persistent: true,
+        html: true,
+        ok: {
+          label: 'Delete',
+          color: 'negative',
+          noCaps: true,
+        },
+        cancel: {
+          color: 'primary',
+          noCaps: true,
+        },
+      }).onOk(() => {
+         deleteEntry(entry.id)
+      }).onCancel(() => {
+          reset()
+      })
+}
+
+
+/*
+  Delete entry
+*/
+
+const deleteEntry = entryId => {
+  const index = entries.value.findIndex(entry=> entry.id === entryId)
+  entries.value.splice(index, 1)
+  $q.notify({
+    message: 'Entry Deleted',
+    position: 'top'
+  })
+}
 
 
 </script>
